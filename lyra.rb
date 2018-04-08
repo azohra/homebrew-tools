0acb0ccf98452fec2a0124baa92cc05fea35eb01
Justin Watts <justin@azohra.com>
2018-04-07 23:56:17 -0400

    Create lyra.rb

---
+++ lyra.rb
@@ -0,0 +1,16 @@
+class Lyra < Formula
+  desc "a lightweight encryption tool that makes protecting your sensitive files easy"
+  homepage "https://github.com/azohra/lyra"
+  url "https://github.com/azohra/lyra/releases/download/v1.0.0/lyra-darwin"
+  sha256 "f0f2ff1995a53579fe188c5e70a1a649926b5bca7026160f619e9d37b35f4a5f"
+
+  bottle :unneeded
+
+  def install
+    bin.install "lyra"
+  end
+
+  test do
+    system "#{bin}/lyra", "-h"
+  end
+end
