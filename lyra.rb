6601bcdec7f5e48643f2bdda2b73772503a17289
Justin Watts <tk8817@users.noreply.github.com>
2018-04-08 00:16:30 -0400

    Update lyra.rb

--- lyra.rb
+++ lyra.rb
@@ -1,7 +1,7 @@
 class Lyra < Formula
   desc "a lightweight encryption tool that makes protecting your sensitive files easy"
   homepage "https://github.com/azohra/lyra"
-  url "https://github.com/azohra/lyra/releases/download/v1.0.0/lyra-darwin-1.0.0.tar.gz"
+  url "https://github.com/azohra/lyra/releases/download/v1.0.0/lyra_darwin_1.0.0.tar.gz"
   sha256 "6d55958eab6cd66ef6682de4ad3306cde80fc357b4b6e690e784415d6ac985f2"
 
   bottle :unneeded
