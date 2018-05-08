0003c56d984b885a46e8caef49205ea466d00e62
Brandon Sam Soon <10151952+brsmsn@users.noreply.github.com>
2018-05-08 14:06:50 -0400

    Update lyra.rb

--- lyra.rb
+++ lyra.rb
@@ -2,7 +2,7 @@ class Lyra < Formula
   desc "a lightweight encryption tool that makes protecting your sensitive files easy"
   homepage "https://github.com/azohra/lyra"
   url "https://github.com/azohra/lyra/releases/download/v1.1.0/lyra_darwin_amd64_v1.1.0.tar.gz"
-  sha256 "fbd8de5a80697b2f9776df80220f726e43d38a66e4a2b25a02e2d2e9ce1ab8e6"
+  sha256 "20f7abeb59d888f825a432efb9a83ec7af1e25f59a744db35781dbdc4bbce6fe"
 
   bottle :unneeded
 
