0ade836db0601c123edbee8a89251010653973a8
Azadeh Bagheri <Azadeh.Bagheri@loblaw.ca>
2019-07-30 15:12:18 -0400

    updated gitrise.rb

--- gitrise.rb
+++ gitrise.rb
@@ -2,7 +2,7 @@ class Gitrise < Formula
   desc "a lightweight bash tool to trigger builds on Bitrise"
   homepage "https://github.com/azohra/gitrise.sh"
   url "https://github.com/azohra/gitrise.sh/archive/v0.1.0.tar.gz"
-  sha256 "3d494d5541527a56972e9f2bd195dbcb89659a35d0bf198972c0cc55de9ffaec"
+  sha256 "d16cfa88e5577e65bbf9ff2971614ef3641bdd10b24f9f5b6d12bd0c0a87eec6"
 
   bottle :unneeded
 
