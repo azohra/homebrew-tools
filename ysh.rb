7504b24c686c60084834a0442133c074136fe1af
Justin Watts <justin@azohra.com>
2026-08-03 01:31:40 -0700

    Publish YAML.sh 1.16.0 (#16)

--- ysh.rb
+++ ysh.rb
@@ -1,8 +1,8 @@
 class Ysh < Formula
   desc "Query and update YAML with one portable shell and AWK file"
   homepage "https://yaml.azohra.com"
-  url "https://github.com/azohra/yaml.sh/releases/download/v1.15.0/ysh", using: :nounzip
-  sha256 "844ba6114628b0a4e4c35b62e61f7f280bdbeb5ca2424dbc8c909cc841575918"
+  url "https://github.com/azohra/yaml.sh/releases/download/v1.16.0/ysh", using: :nounzip
+  sha256 "70b2209667a6987dc09b72c8450b64eeab985d76edfa3138109b2f8410789dca"
   license "MIT"
 
   def install
